43f6d5b30e3d87be240493c471fb14a8b1bbbed9
veerendra h <veerendra h@skycliffitdomain.com>
2021-03-18 14:44:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+## Scratch Card App
+
+[![BUILD](https://img.shields.io/badge/Build-Passing-<COLOR>.svg)](https://github.com/vijayinyoutube/scratcherapp)  [![vijaycreations](https://img.shields.io/website-up-vijaycreations-green-orange/http/cv.lbesson.qc.to.svg)](https://www.youtube.com/channel/UCBC_Z7jla1GSITcqLKAtPxQ) [![Repo Status](https://img.shields.io/badge/RepoStatus-Active-yellow.svg)](https://github.com/vijayinyoutube/scratcherapp) [![MIT license](https://img.shields.io/badge/License-MIT-red.svg)](https://github.com/vijayinyoutube/scratcherapp) [![Flutter](https://img.shields.io/badge/Built_using-Flutter-blue.svg)](https://github.com/vijayinyoutube/scratcherapp)
+
+An open source sample google pay scratch card demo app
+
+## Flutter
+[Flutter](https://flutter.dev/) is a crossplatfrom UI tool kit that helps in developing native Android and iOS app from a single code base.
+
+
+## App
+
+We have developed a similar feature that resembles the  Gpay scratch card. In this app we have made use of Scratcher Widget to make this possible.
+
+Check out the video Tutorial👉: https://youtu.be/GP2vguH2KEk
+
+Visit My Channel for more Flutter Tutorials 👉: https://www.youtube.com/channel/UCBC_Z7jla1GSITcqLKAtPxQ 
+
+## Dependencies
+```pubspec.yaml
+scratcher: "^1.4.0"
+confetti: ^0.5.4+1
+```
+
+## Sample Results
+
+<img src="https://user-images.githubusercontent.com/58719230/90313932-d0edf700-df2d-11ea-9843-05a7ca162aa1.png" width="200"  height="325"> <img src="https://user-images.githubusercontent.com/58719230/90313931-cd5a7000-df2d-11ea-952f-93cde0d7c30b.png" width="200"  height="325"> <img src="https://user-images.githubusercontent.com/58719230/90597214-413a9800-e20e-11ea-9357-3eb03439727f.png" width="200"  height="325"> <img src="https://user-images.githubusercontent.com/58719230/90313936-d2b7ba80-df2d-11ea-94d7-e3da17cf96ab.png" width="200"  height="325"> 
+
+## License
+
+```
+MIT License
+
+Copyright (c) 2020 Vijay R
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
+
+
---
+++ lib/Homepage.dart
@@ -0,0 +1,96 @@
+import 'package:confetti/confetti.dart';
+import 'package:flutter/material.dart';
+import 'package:scratcher/widgets.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ConfettiController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new ConfettiController(
+      duration: new Duration(seconds: 2),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Scratcher(
+          brushSize: 50,
+          threshold: 75,
+          color: Colors.red,
+          image: Image.asset(
+            "assets/images/outerimage.png",
+            fit: BoxFit.fill,
+          ),
+          onChange: (value) => print("Scratch progress: $value%"),
+          onThreshold: () => _controller.play(),
+          child: Container(
+            height: 300,
+            width: 300,
+            color: Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/newimage.png",
+                  fit: BoxFit.contain,
+                  width: 150,
+                  height: 150,
+                ),
+                Column(
+                  children: [
+                    ConfettiWidget(
+                      blastDirectionality: BlastDirectionality.explosive,
+                      confettiController: _controller,
+                      particleDrag: 0.05,
+                      emissionFrequency: 0.05,
+                      numberOfParticles: 100,
+                      gravity: 0.05,
+                      shouldLoop: false,
+                      colors: [
+                        Colors.green,
+                        Colors.red,
+                        Colors.yellow,
+                        Colors.blue,
+                      ],
+                    ),
+                    Container(
+                      child: Text(
+                        "Dear MalaShree",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                    Container(
+                      child: Text(
+                        "Many More Happy \n"
+                            "returns of the day",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
